8552b7c2753873f39c5ff555f20d8386ea613edc
Joaquin <sol19008@byui.edu>
2020-11-09 18:23:33 -0300

    agregando cambios en los puertos 3000, estaban en el 3002

--- dashboard/dashboard/src/App.js
+++ dashboard/dashboard/src/App.js
@@ -15,23 +15,23 @@ class App extends Component {
     super(props);
     this.state = {
       metrics: [],
-      metricsUsers:[],
+      metricsUsers: [],
       categories: [],
-      products:[],
-      users:[]
+      products: [],
+      users: []
     };
   }
 
   componentDidMount() {
 
-    fetch('http://192.168.1.38:3002/api/products')
+    fetch('http://192.168.1.38:3000/api/products')
       .then(response => response.json())
       .then(products => {
 
-        console.log( 
-          products.data.map(product =>(product.name)))
+        console.log(
+          products.data.map(product => (product.name)))
 
-          console.log(products.data[0].imagen)  
+        console.log(products.data[0].imagen)
         this.setState({
           metrics: [
             {
@@ -49,14 +49,14 @@ class App extends Component {
           ],
           categories: products.meta.categories
           ,
-          products:products.data
+          products: products.data
         },
-        
+
         )
       })
       .catch(error => console.log(error));
 
-      fetch('http://192.168.1.38:3002/api/users')
+    fetch('http://192.168.1.38:3000/api/users')
       .then(response => response.json())
       .then(users => {
         this.setState({
@@ -70,165 +70,165 @@ class App extends Component {
           ],
           users: users.data
         },
-        
+
         )
       })
       .catch(error => console.log(error));
   }
 
 
-componentDidUpdate() {
+  componentDidUpdate() {
 
-}
+  }
 
-render(){
-  return (
+  render() {
+    return (
 
-    <div id="wrapper">
-
-		
-
-    <Sidebar />
-		<div id="content-wrapper" className="d-flex flex-column">
-
-			<div id="content">
-
-        <Navbar />
-        <Route exact path="/products" component={() => <Products products={this.state.products}  />}/>
-        <Route exact path="/users" component={() => <Users users={this.state.users}  />}/>
+      <div id="wrapper">
 
 
-				<div className="container-fluid">
 
-					<div className="d-sm-flex align-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-					</div>
+        <Sidebar />
+        <div id="content-wrapper" className="d-flex flex-column">
 
-          <div className="row">
+          <div id="content">
+
+            <Navbar />
+            <Route exact path="/products" component={() => <Products products={this.state.products} />} />
+            <Route exact path="/users" component={() => <Users users={this.state.users} />} />
 
 
-          { 
-                  this.state.metrics.length ? 
+            <div className="container-fluid">
+
+              <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+              </div>
+
+              <div className="row">
+
+
+                {
+                  this.state.metrics.length ?
                     this.state.metrics.map((metric, index) =>
-                      <Topbar 
-                        color= { metric.color }
-                        title= { metric.title }
-                        value= { metric.value }
-                        iconClass= { metric.iconClass }
-                        key= { `metric-${index}`}
+                      <Topbar
+                        color={metric.color}
+                        title={metric.title}
+                        value={metric.value}
+                        iconClass={metric.iconClass}
+                        key={`metric-${index}`}
                       />
                     )
-                  :
+                    :
                     <p>Cargando métricas...</p>
-                  
-                    
+
+
                 }
                 {
-                  this.state.metricsUsers.length ? 
-                  this.state.metricsUsers.map((metricsUser, index) =>
-                    <Topbar 
-                      color= { metricsUser.color }
-                      title= { metricsUser.title }
-                      value= { metricsUser.value }
-                      iconClass= { metricsUser.iconClass }
-                      key= { `metric-${index}`}
-                    />
-                  )
-                  :
-                  <p>Cargando métricas...</p>
-                }
-          </div>
-
-          
-
-
-					<div className="row">
-
-						<div className="col-lg-6 mb-4">
-							<div className="card shadow mb-4">
-								<div className="card-header py-3">
-									<h6 className="m-0 font-weight-bold text-primary">Ultimo producto subido</h6>
-								</div>
-								<div className="card-body">
-									<div className="text-center">
-										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '25rem'}} src="../images/product_dummy.svg" alt="image dummy"/>
-                    {
-                    
-                    /* "../images/product_dummy.svg" 
-                    this.state.products[0].imagen*/}
-									</div>
-									<p>Aqui podras obtenes las metricas para ver el rendimiento de tus productos.</p>
-									<a target="_blank" rel="nofollow" href="/">View product detail</a>
-								</div>
-							</div>
-						</div>
-
-
-						<BoxCategories categories={this.state.categories} />
-            
-
-					</div>
-
-          <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                  <h1 className="h3 mb-0 text-gray-800">Todos los productos</h1>
-                </div>
-
-            <div className="row">
-              <div className="col-lg-12 mb-4">
-                <div className="card shadow mb-4">
-                  <div className="card-body">
-
-                  <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Category</th>
-                </tr>
-              </thead>
-
-                  { 
-                  this.state.products.length ? 
-                    this.state.products.map((product, index) =>
-                      <Table 
-                        id= { product.id }
-                        name= { product.name }
-                        price= { product.price }
-                        category= { product.category }
-                        key= { `product-${index}`}
+                  this.state.metricsUsers.length ?
+                    this.state.metricsUsers.map((metricsUser, index) =>
+                      <Topbar
+                        color={metricsUser.color}
+                        title={metricsUser.title}
+                        value={metricsUser.value}
+                        iconClass={metricsUser.iconClass}
+                        key={`metric-${index}`}
                       />
                     )
-                  :
-                    <p>Cargando productos...</p>   
+                    :
+                    <p>Cargando métricas...</p>
                 }
-                </table>
-                  
-                  </div>
-                </div>				
               </div>
+
+
+
+
+              <div className="row">
+
+                <div className="col-lg-6 mb-4">
+                  <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                      <h6 className="m-0 font-weight-bold text-primary">Ultimo producto subido</h6>
+                    </div>
+                    <div className="card-body">
+                      <div className="text-center">
+                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: '25rem' }} src="../images/product_dummy.svg" alt="image dummy" />
+                        {
+
+                    /* "../images/product_dummy.svg" 
+                    this.state.products[0].imagen*/}
+                      </div>
+                      <p>Aqui podras obtenes las metricas para ver el rendimiento de tus productos.</p>
+                      <a target="_blank" rel="nofollow" href="/">View product detail</a>
+                    </div>
+                  </div>
+                </div>
+
+
+                <BoxCategories categories={this.state.categories} />
+
+
+              </div>
+
+              <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 className="h3 mb-0 text-gray-800">Todos los productos</h1>
+              </div>
+
+              <div className="row">
+                <div className="col-lg-12 mb-4">
+                  <div className="card shadow mb-4">
+                    <div className="card-body">
+
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Category</th>
+                          </tr>
+                        </thead>
+
+                        {
+                          this.state.products.length ?
+                            this.state.products.map((product, index) =>
+                              <Table
+                                id={product.id}
+                                name={product.name}
+                                price={product.price}
+                                category={product.category}
+                                key={`product-${index}`}
+                              />
+                            )
+                            :
+                            <p>Cargando productos...</p>
+                        }
+                      </table>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
             </div>
 
+          </div>
 
-				</div>
-
-			</div>
-
-			<footer className="sticky-footer bg-white">
-				<div className="container my-auto">
-					<div className="copyright text-center my-auto">
-						<span>Copyright &copy; Dashboard 2020</span>
-					</div>
-				</div>
-			</footer>
+          <footer className="sticky-footer bg-white">
+            <div className="container my-auto">
+              <div className="copyright text-center my-auto">
+                <span>Copyright &copy; Dashboard 2020</span>
+              </div>
+            </div>
+          </footer>
 
 
-		</div>
+        </div>
 
 
-	</div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default App;
--- dashboard/dashboard/src/components/Users.js
+++ dashboard/dashboard/src/components/Users.js
@@ -1,52 +1,52 @@
 import React from 'react'
 import Table from './Table'
 
-export default (props)=>{
-    console.log("usuarios")
-    console.log(props.users)
+export default (props) => {
+  console.log("usuarios")
+  console.log(props.users)
 
-    return (
-        <div className="">
-            <h1>Listado de usuarios</h1>
+  return (
+    <div className="">
+      <h1>Listado de usuarios</h1>
 
-            <div className="row">
-              <div className="col-lg-12 mb-4">
-                <div className="card shadow mb-4">
-                  <div className="card-body">
+      <div className="row">
+        <div className="col-lg-12 mb-4">
+          <div className="card shadow mb-4">
+            <div className="card-body">
 
-                  <table className="table">
+              <table className="table">
                 <thead>
-                <tr>
+                  <tr>
                     <th scope="col">ID</th>
                     <th scope="col">Name</th>
                     <th scope="col">Lastname</th>
                     <th scope="col">Email</th>
-                </tr>
-              </thead>
-            { 
-                  props.users.length ? 
+                  </tr>
+                </thead>
+                {
+                  props.users.length ?
                     props.users.map((user, index) =>
-                      <Table 
-                        id= { user.id }
-                        name= { user.first_name }
-                        price= { user.last_name }
-                        category= { user.email }
-                        key= { `user-${index}`}
+                      <Table
+                        id={user.id}
+                        name={user.name}
+                        price={user.last_name}
+                        category={user.email}
+                        key={`user-${index}`}
                       />
                     )
-                  :
-                    <p>Cargando usuarios...</p>   
+                    :
+                    <p>Cargando usuarios...</p>
                 }
 
-                    </table>
-                  
-                  </div>
-                </div>				
-              </div>
-            </div>
+              </table>
 
-            
+            </div>
+          </div>
+        </div>
       </div>
 
-    )
+
+    </div>
+
+  )
 }
